cfad046cfd5ee04181aa8b8a26b004ac41b62712
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-01-24 13:49:39 +0100

    Chip component updated

--- lib/screens/component.dart
+++ lib/screens/component.dart
@@ -49,7 +49,6 @@ class ComponentClass extends StatelessWidget {
                   CircleIcon.error(),
                 ],
               ),
-              //////////3
             ],
           ),
         ),
