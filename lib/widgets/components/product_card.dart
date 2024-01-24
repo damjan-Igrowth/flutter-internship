263bb27af610d38367731d87c5c0f6e3255ab796
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-01-24 09:18:26 +0100

    Changes on product card made

--- lib/widgets/components/product_card.dart
+++ lib/widgets/components/product_card.dart
@@ -16,66 +16,71 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     double maxWidth = 168;
     double maxHeight = 125.516;
-    return Card(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight),
-        child: Column(
-          children: [
-            Container(
-              height:
-                  148.148, // A RenderFlex overflowed by 83 pixels on the bottom. << pitanje
-              padding: const EdgeInsets.fromLTRB(3, 11, 2, 11.632),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(11.111),
-                  topRight: Radius.circular(11.111),
+    return SingleChildScrollView(
+      child: Card(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          child: Column(
+            children: [
+              Container(
+                // height: 148.148,
+                padding: const EdgeInsets.fromLTRB(3, 11, 2, 11.632),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(11.111),
+                    topRight: Radius.circular(11.111),
+                  ),
+                  color: Color(0xFFF6F6F6),
                 ),
-                color: Color(0xFFF6F6F6),
+                child: Container(
+                  constraints: BoxConstraints(maxHeight: maxHeight),
+                  child: Image.asset(
+                    image,
+                    width: maxWidth,
+                    height: maxHeight,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              child: Image.asset(
-                image,
-                width: maxWidth,
-                height: maxHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      title,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        color: Color(0xFF181E25),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                        fontStyle: FontStyle.normal,
-                        fontFamily: 'Inter',
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        title,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Color(0xFF181E25),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                          fontFamily: 'Inter',
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 4),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      price,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        color: Color(0xFFEE2636),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w700,
-                        fontStyle: FontStyle.normal,
-                        fontFamily: 'Inter',
+                    const SizedBox(height: 4),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        price,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Color(0xFFEE2636),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.normal,
+                          fontFamily: 'Inter',
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
