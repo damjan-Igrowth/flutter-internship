f7576697668b933a06fbe01af69d7c3943e7f816
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-01-25 13:31:55 +0100

    Merge feature/int-6-assignment-card-component into main

--- lib/widgets/components/chip_component.dart
+++ lib/widgets/components/chip_component.dart
@@ -2,24 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_internship/helpers/spacer_width.dart';
 
 enum ChipType { flat, clean }
-import 'package:flutter_internship/helpers/spacer_width.dart';
-
-enum ChipType { flat, clean }
 
 class ChipComponent extends StatelessWidget {
   final ChipType chipType;
   final IconData? icon;
   final String label;
-class ChipComponent extends StatelessWidget {
-  final ChipType chipType;
-  final IconData? icon;
-  final String label;
 
-  const ChipComponent({
   const ChipComponent({
     super.key,
-    required this.chipType,
-    this.icon,
     required this.chipType,
     this.icon,
     required this.label,
