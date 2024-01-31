5e9d58c213267f3adef5c74df4c2739ae68566c1
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-01-31 15:09:59 +0100

    StopWatch laps updated

--- lib/screens/stop_watch.dart
+++ lib/screens/stop_watch.dart
@@ -10,34 +10,6 @@ class StopWatch extends StatefulWidget {
   State<StopWatch> createState() => _StopWatchState();
 }
 
-Duration durationFromLapString(String lap) {
-  List<String> parts = lap.split(':');
-  int minutes = int.parse(parts[0]);
-  List<String> secondsAndMilliseconds = parts[1].split('.');
-  int seconds = int.parse(secondsAndMilliseconds[0]);
-  int milliseconds = int.parse(
-      secondsAndMilliseconds.length > 1 ? secondsAndMilliseconds[1] : '0');
-
-  return Duration(
-      minutes: minutes, seconds: seconds, milliseconds: milliseconds);
-}
-
-double lapDifference(String lap1, String lap2) {
-  final lap1Duration = durationFromLapString(lap1);
-  final lap2Duration = durationFromLapString(lap2);
-  return lap2Duration.inMilliseconds.toDouble() -
-      lap1Duration.inMilliseconds.toDouble();
-}
-
-List<double> calculateLapDifferences(List<String> laps) {
-  List<double> differences = [];
-  for (int i = 1; i < laps.length; i++) {
-    double difference = lapDifference(laps[i - 1], laps[i]);
-    differences.add(difference);
-  }
-  return differences;
-}
-
 class _StopWatchState extends State<StopWatch> {
   int hundredths = 0, seconds = 0, minutes = 0;
   String digitHundredths = '00', digitSeconds = '00', digitMinutes = '00';
@@ -105,15 +77,8 @@ class _StopWatchState extends State<StopWatch> {
 
   @override
   Widget build(BuildContext context) {
-    final differences = calculateLapDifferences(laps);
-    final longestDifference = differences.isNotEmpty
-        ? differences.reduce((a, b) => a > b ? a : b)
-        : 0.0;
-    final shortestDifference = differences.isNotEmpty
-        ? differences.reduce((a, b) => a < b ? a : b)
-        : 0.0;
-
     return Container(
+      constraints: const BoxConstraints(maxHeight: 844, maxWidth: 388),
       color: const Color(0xFF0E111C),
       child: Column(
         children: [
@@ -127,21 +92,23 @@ class _StopWatchState extends State<StopWatch> {
                   color: Color(0xFFFFFFFF),
                 ),
                 SizedBox(width: 8),
-                Text('Stopwatch',
-                    style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontFamily: 'Inter',
-                        fontSize: 20,
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.w600,
-                        decoration: TextDecoration.none)),
+                Text(
+                  'Stopwatch',
+                  style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontFamily: 'Inter',
+                      fontSize: 20,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w600,
+                      decoration: TextDecoration.none),
+                ),
               ],
             ),
           ),
           const SizedBox(height: 48),
           Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 54),
+              padding: const EdgeInsets.fromLTRB(60, 0, 61, 0),
               child: Text(
                 '$digitMinutes:$digitSeconds.$digitHundredths',
                 overflow: TextOverflow.ellipsis,
@@ -155,11 +122,11 @@ class _StopWatchState extends State<StopWatch> {
               ),
             ),
           ),
-          const SizedBox(height: 20),
+          const SizedBox(height: 40),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 44),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 _CircularButton(
                   onPressed: () {
@@ -189,23 +156,29 @@ class _StopWatchState extends State<StopWatch> {
             ),
           ),
           const SizedBox(height: 40),
-          SingleChildScrollView(
+          Expanded(
             child: Container(
-              height: 350,
               decoration: const BoxDecoration(color: Color(0xFF0B0D15)),
               child: ListView.builder(
                 itemCount: laps.length,
                 itemBuilder: (context, index) {
                   final reversedIndex = laps.length - 1 - index;
+                  final differences = calculateLapDifferences(laps);
+                  final longestDifference = differences.isNotEmpty
+                      ? differences.reduce((a, b) => a > b ? a : b)
+                      : 0.0;
+                  final shortestDifference = differences.isNotEmpty
+                      ? differences.reduce((a, b) => a < b ? a : b)
+                      : 0.0;
 
                   Color textColor = const Color(0xFFFFFFFF);
 
-                  if (index < differences.length) {
-                    if (differences[index] == longestDifference) {
+                  if (reversedIndex > 0 && reversedIndex < differences.length) {
+                    if (differences[reversedIndex - 1] == longestDifference) {
                       textColor = const Color(0xFFF13F3F);
                     }
 
-                    if (differences[index] == shortestDifference) {
+                    if (differences[reversedIndex - 1] == shortestDifference) {
                       textColor = const Color(0xFF4AE575);
                     }
                   }
@@ -215,27 +188,12 @@ class _StopWatchState extends State<StopWatch> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          'Lap ${reversedIndex + 1}',
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: 'Inter',
-                              fontSize: 16,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w500,
-                              decoration: TextDecoration.none),
+                        _LapText(
+                          lapText: 'Lap ${reversedIndex + 1}',
+                          textColor: textColor,
                         ),
-                        Text(
-                          laps[reversedIndex],
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: 'Inter',
-                              fontSize: 16,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w500,
-                              decoration: TextDecoration.none),
-                        ),
+                        _LapText(
+                            lapText: laps[reversedIndex], textColor: textColor),
                       ],
                     ),
                   );
@@ -245,6 +203,60 @@ class _StopWatchState extends State<StopWatch> {
           ),
         ],
       ),
+    );
+  }
+}
+
+Duration durationFromLapString(String lap) {
+  List<String> parts = lap.split(':');
+  int minutes = int.parse(parts[0]);
+  List<String> secondsAndMilliseconds = parts[1].split('.');
+  int seconds = int.parse(secondsAndMilliseconds[0]);
+  int milliseconds = int.parse(
+      secondsAndMilliseconds.length > 1 ? secondsAndMilliseconds[1] : '0');
+
+  return Duration(
+      minutes: minutes, seconds: seconds, milliseconds: milliseconds);
+}
+
+double lapDifference(String lap1, String lap2) {
+  final lap1Duration = durationFromLapString(lap1);
+  final lap2Duration = durationFromLapString(lap2);
+  return (lap2Duration.inMilliseconds.toDouble() -
+          lap1Duration.inMilliseconds.toDouble()) /
+      10;
+}
+
+List<double> calculateLapDifferences(List<String> laps) {
+  List<double> differences = [];
+  for (int i = 1; i < laps.length; i++) {
+    double difference = lapDifference(laps[i - 1], laps[i]);
+    differences.add(difference);
+  }
+  return differences;
+}
+
+class _LapText extends StatelessWidget {
+  final String lapText;
+  final Color textColor;
+
+  const _LapText({
+    required this.lapText,
+    required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      lapText,
+      textAlign: TextAlign.right,
+      style: TextStyle(
+          color: textColor,
+          fontFamily: 'Inter',
+          fontSize: 16,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.w500,
+          decoration: TextDecoration.none),
     );
   }
 }
