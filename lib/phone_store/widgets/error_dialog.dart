7076e96e9c365b07831be07eb2414f03ce31c4fc
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-02-01 10:29:38 +0100

    PhoneStore

---
+++ lib/phone_store/widgets/error_dialog.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_internship/components/widgets/circle_icon.dart';
+
+class ErrorDialog extends StatelessWidget {
+  const ErrorDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      surfaceTintColor: const Color(0xFFFFFFFF),
+      contentPadding: const EdgeInsets.all(16),
+      content: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircleIcon.error(),
+          SizedBox(height: 16),
+          Text(
+            'Error!',
+            style: TextStyle(
+              color: Color(0xFF000000),
+              fontFamily: 'Inter',
+              fontSize: 20,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 12),
+          Text(
+            'Something went wrong while editing product!',
+            style: TextStyle(
+              color: Color(0xFF787878),
+              fontFamily: 'Inter',
+              fontSize: 14,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Try again',
+              style: TextStyle(
+                color: Color(0xFFFFFFFF),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
