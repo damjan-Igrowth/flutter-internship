3023e219f6a343d5a20d815ebf28f6ad9f1c09cf
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-02-08 14:05:33 +0100

    Disabled and enabled button updates

--- lib/phone_shop/widgets/components/fill_button.dart
+++ lib/phone_shop/widgets/components/fill_button.dart
@@ -2,32 +2,45 @@ import 'package:flutter/material.dart';
 
 class FillButton extends StatelessWidget {
   final String buttonText;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
 
   const FillButton({
     super.key,
     required this.buttonText,
-    required this.onTap,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
+    final Color buttonTextColor =
+        onTap == null ? const Color(0xFFCED6E2) : const Color(0xFFFFFFFF);
+    final String buttonFontFamily = onTap == null ? 'Poppins' : 'Inter';
+
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF34A4E3),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return const Color(0xFF8A93A0);
+            }
+            return const Color(0xFF34A4E3);
+          },
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
         ),
       ),
       onPressed: onTap,
       child: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
           child: Text(
             buttonText,
-            style: const TextStyle(
-              color: Color(0xFFFFFFFF),
-              fontFamily: 'Inter',
+            style: TextStyle(
+              color: buttonTextColor,
+              fontFamily: buttonFontFamily,
               fontSize: 16,
               fontStyle: FontStyle.normal,
               fontWeight: FontWeight.w600,
