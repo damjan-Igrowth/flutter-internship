b1c9921c54aa8e0b5d2d1adb53862cf2cb7e5020
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-02-09 16:15:40 +0100

    Save

--- lib/phone_shop/widgets/components/gallery.dart
+++ lib/phone_shop/widgets/components/gallery.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_internship/phone_shop/widgets/data/phones_list.dart';
-import 'package:flutter_internship/phone_shop/widgets/models/phone.dart';
 
 class Gallery extends StatelessWidget {
-  const Gallery({super.key});
+  final List<String> images;
+
+  const Gallery({super.key, required this.images});
 
   @override
   Widget build(BuildContext context) {
     Widget productListView;
 
-    productListView = GridView.builder(
+    productListView = ListView.builder(
       scrollDirection: Axis.horizontal,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        crossAxisSpacing: 16,
-      ),
-      itemCount: phonesList.length,
+      itemCount: images.length,
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.only(left: 20),
-          child: Phone(
-            image: phonesList[index].image,
+          child: _ImageItem(
+            image: images[index],
           ),
         );
       },
     );
-
     return SizedBox(
       height: 328,
       child: productListView,
+    );
+  }
+}
+
+class _ImageItem extends StatelessWidget {
+  final String image;
+
+  const _ImageItem({
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 328,
+      height: 328,
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color(0xFFF3F3F3),
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Image.network(
+        image,
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
