021995eba6b53721f86254b56968d1a9eb6253a3
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-02-11 22:03:33 +0100

    Section created

---
+++ lib/phone_shop/widgets/components/section.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Section extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const Section({super.key, required this.title, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _Title(title: title),
+        const SizedBox(height: 16),
+        child,
+      ],
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  final String title;
+
+  const _Title({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        color: Color(0xFF121212),
+        fontFamily: 'Inter',
+        fontSize: 16,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+  }
+}
