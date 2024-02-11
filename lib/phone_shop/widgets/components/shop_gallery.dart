a4931759c8603a715344d2333100b25409e78029
vukasinpetkovicgreenstate <vukasin.petkovic@greenstate.ch>
2024-02-11 22:20:03 +0100

    Gallery changes

--- lib/phone_shop/widgets/components/shop_gallery.dart
+++ lib/phone_shop/widgets/components/shop_gallery.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-class Gallery extends StatelessWidget {
+class ShopGallery extends StatelessWidget {
   final List<String> images;
 
-  const Gallery({super.key, required this.images});
+  const ShopGallery({super.key, required this.images});
 
   @override
   Widget build(BuildContext context) {
@@ -37,20 +37,11 @@ class _ImageItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 328,
-      height: 328,
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: const Color(0xFFF3F3F3),
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(8),
-      ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
       child: Image.network(
         image,
-        fit: BoxFit.cover,
+        fit: BoxFit.contain,
       ),
     );
   }
